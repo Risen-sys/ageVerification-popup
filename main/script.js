626f9b83b6cc868cb703bd4dc1d27b7db5231e9f
Matthew Silbernagel <118539484+Risen-sys@users.noreply.github.com>
2023-02-28 19:59:58 -0600

    Add files via upload

---
+++ main/script.js
@@ -0,0 +1,19 @@
+overAge = function () {
+  $('#age-verify').addClass('hidden');
+}
+
+underAge = function () {
+  $('#age-verify').addClass('under');
+}
+
+goBack = function () {
+    window.history.back();
+}
+
+
+
+
+
+
+
+
